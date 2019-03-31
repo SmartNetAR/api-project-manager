b35294b0dbad6d6077c239fd104a217d190594be
Leonardo Casales <leonardo@smartnet.com.ar>
2019-03-31 19:02:31 -0300

    passport, time zone, user register

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Validator;
+
+class AuthController extends Controller
+{
+
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email',
+            'password' => 'required',
+            'confirm_password' => 'required|same:password',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error'=>$validator->errors()], 422);
+        }
+
+        $input = $request->all();
+        $input['password'] = bcrypt($request->get('password'));
+        $user = User::create($input);
+        $token =  $user->createToken('MyApp')->accessToken;
+
+        return response()->json([
+            'token' => $token,
+            'user' => $user
+        ], 200);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,14 +4,15 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-
+use Laravel\Passport\Passport;
 class AuthServiceProvider extends ServiceProvider
 {
     /**
      * The policy mappings for the application.
      *
      * @var array
-     */
+     */    
+
     protected $policies = [
         // 'App\Model' => 'App\Policies\ModelPolicy',
     ];
@@ -24,7 +25,7 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
+        Passport::routes();
         //
     }
 }

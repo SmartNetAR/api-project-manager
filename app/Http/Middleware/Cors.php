73ebf93116f923cb09442a30f430db3128d8c35d
Leonardo Casales <leonardo@smartnet.com.ar>
2019-04-09 14:07:20 -0300

    cors solved

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,18 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', '*')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
+        header("Access-Control-Allow-Origin: *");
+        $headers = [
+            'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE, OPTIONS' ,
+            'Access-Control-Allow-Headers' => ' Origin, Content-Type, Authorization, X-Auth-Token'
+        ] ;
+        if ( $request->getMethod() == "OPTIONS" ) {
+            return response()->json('OK', 200, $headers );
+        }
+        $response = $next( $request ) ;
+        foreach ( $headers as $key => $value ) {
+            $response->header( $key, $value ) ;
+        }
+        return $response ;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -13,8 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::post('register', 'AuthController@register');
-Route::post('login', 'AuthController@login');
+    Route::post('register', 'AuthController@register');
+    Route::post('login', 'AuthController@login');
+
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::get('profile', 'AuthController@profile');
